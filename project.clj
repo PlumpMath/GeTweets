986dbcaa7ebf974fc69aef13ded1702bbee445d9
rod <rhodri.pugh@boxuk.com>
2012-04-20 08:44:29 +0100

    all pages of a users tweets now fetched with max page size of 200 that is allowed by twitter

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 
-(defproject getweets "0.0.1"
-  :description "Fetches all tweets by user"
+(defproject getweets "1.0.0"
+  :description "Fetches all tweets by user as JSON"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [cheshire "4.0.0"]]
   :main getweets.core)
--- src/getweets/core.clj
+++ src/getweets/core.clj
@@ -7,19 +7,24 @@
        twitter "https://api.twitter.com/1/statuses/user_timeline.json?screen_name=%s&page=%d&count=%d&trim_user=1&include_rts=1")
 
 (def ^{:doc "The number of tweets to fetch in each page of results"}
-       per-page 25)
+       per-page 200)
 
-(defn- fetch
-  "Fetch a page of tweets"
-  ([screen_name] (fetch screen_name 1))
-  ([screen_name page]
+;; Public
+
+(defn fetch
+  "Fetch a users tweets by screen_name.  Just use the single arity version of the function, the multiple
+   artity version is used recursively to fetch pages of tweets."
+  ([screen_name] (fetch screen_name 1 []))
+  ([screen_name page all-tweets]
     (let [url (format twitter screen_name page per-page)
-          tweets (parse-stream (reader url) true)]
-      (if (< (count tweets) per-page)
-          (recur screen_name (inc page))))))
+          new-tweets (parse-stream (reader url) true)
+          tweets (concat all-tweets new-tweets)]
+      (if (< (count new-tweets) per-page)
+          tweets
+          (recur screen_name (inc page) tweets)))))
 
 ;; Main
 
 (defn -main [& [screen_name]]
-  (fetch screen_name))
+  (prn (fetch screen_name)))
 
